6a324e75942dc7c1b13087cf399741a808270040
Noelia R <noeliaromga@gmail.com>
2022-04-26 08:51:57 +0200

    add react started kit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+
+import "../styles/App.scss";
+//import logo from '../images/logo.jpg';
+//import { useEffect, useState } from "react";
+//import ls from "../services/localStorage";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="title">Título de mi página</h1>
+        <img src={logo} title="Adalab" alt="Logo de Adalab" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
